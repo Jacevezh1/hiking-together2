24247130af98f071207fe89b0be823aed07b0883
Javier Herrera <jaaah0010@gmail.com>
2021-11-29 13:26:39 -0600

    login y signup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-// 1. Imports
 
+// 1. Imports
 const express = require('express')
 const app = express()
 
@@ -9,35 +9,35 @@ const path = require('path')
 
 
 
-// 2. Middlewares ( Static files - HTML CSS JS IMAGES )
-
+// 2. Middlewares
+// Static files - HTML CSS JS IMAGES
 app.use(express.static(path.join(__dirname, 'public')))
+
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
-
-// To use req.body (Para poder acceder a los datos de los formularios que mis usuarios utilicen)
+// To use req.body
 app.use(express.urlencoded({ extended: true }))
 
 
-// 3. Layout Middleware (Importante para saber si un usrio esta loggeado y checar en las vistas)
+
+
+// 3. Layout Middleware
+// Importante para saber si un usrio esta loggeado y checar en las vistas
 
 
 
 
+// 4.Routes
 
-// 4. Routes
-
-// a) Home
+// Home
 app.use('/', require('./routes'))
 
+// Login y signup
+app.use('/auth', require('./routes/auth.router'))
 
 
-// 5. Export
 
+// Export
 module.exports = app
-
-
-
-
--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,10 +1,33 @@
-// 1. Import
+// 1. Import (Express, y el router, y controllers)
 
+const router = require('express').Router()
+
+const { 
+    getSignup, 
+    postSignup,
+    getLogin,
+    postLogin
+
+    
+} = require('./../controllers/auth.controller') 
 
 
 // 2. Routes
+
+// a. Signup
+
+router.get('/signup', getSignup)
+router.post('/signup', postSignup)
+
+
+// b. Login
+
+router.get('/login', getLogin)
+router.post('/login', postLogin)
+
 
 
 
 // 3. Export
 
+module.exports = router
