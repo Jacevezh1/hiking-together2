c87e95492ae87b625b9c8776c8e4c725b70cf50a
Javier Herrera <jaaah0010@gmail.com>
2021-11-29 13:49:38 -0600

    login-signup

--- src/app.js
+++ src/app.js
@@ -1,43 +1,41 @@
-
 // 1. Imports
+
 const express = require('express')
 const app = express()
-
+const hbs = require("hbs")
 require('dotenv/config')
 
 const path = require('path')
 
 
 
-// 2. Middlewares
-// Static files - HTML CSS JS IMAGES
+// 2. Middlewares ( Static files - HTML CSS JS IMAGES )
+
 app.use(express.static(path.join(__dirname, 'public')))
-
-
+hbs.registerPartials(__dirname + "/views/partials")
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
-// To use req.body
+
+// To use req.body (Para poder acceder a los datos de los formularios que mis usuarios utilicen)
 app.use(express.urlencoded({ extended: true }))
 
 
-
-
-// 3. Layout Middleware
-// Importante para saber si un usrio esta loggeado y checar en las vistas
+// 3. Layout Middleware (Importante para saber si un usrio esta loggeado y checar en las vistas)
 
 
 
 
-// 4.Routes
 
-// Home
+// 4. Routes
+
+// a) Home
 app.use('/', require('./routes'))
 
-// Login y signup
+// b) Login y signup
 app.use('/auth', require('./routes/auth.router'))
 
 
+// 5. Export
 
-// Export
 module.exports = app
